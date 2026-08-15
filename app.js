1f94c83adf4eede98839cc9fae4d680f7436dfef
Paul Uncangco <7192265+islanderTron@users.noreply.github.com>
2026-08-15 19:15:39 -0400

    Fixed the mongoose with async

--- app.js
+++ app.js
@@ -40,11 +40,11 @@ var planetModel = mongoose.model('planets', dataSchema);
 
 
 
-app.post('/planet',   function(req, res) {
+app.post('/planet', async function(req, res) {
    // console.log("Received Planet ID " + req.body.id)
     planetModel.findOne({
         id: req.body.id
-    }, function(err, planetData) {
+    }, await function(err, planetData) {
         if (err) {
             alert("Ooops, We only have 9 planets and a sun. Select a number from 0 - 9")
             res.send("Error in Planet Data")
